980205b7256fca717c0647929a13911dcca72b9e
VitorAzevedo76 <49193024+VitorAzevedo76@users.noreply.github.com>
2020-03-03 22:57:32 +0100

    Update test_class.java

--- hello_Test/src/hello_Test/test_class.java
+++ hello_Test/src/hello_Test/test_class.java
@@ -5,7 +5,7 @@ public class test_class
 	public static void main( String []args ) 
 	{
 		System.out.println("Hello. This is a test."); 
-		System.out.print("Vitor ha estado aqui");
+		System.out.print("");
 		System.out.println("Interesante.No entiendo why no me salen tus updates en tu rep."); 
 		
 	}
