6c1d694f2ff864f48876bebc9cc2d088da16c204
vitorazevedo <vitorazevedo@localhost>
2020-03-03 22:14:15 +0100

    prueba añadir clase

---
+++ hello_Test/src/hello_Test/kk.java
@@ -0,0 +1,10 @@
+package hello_Test;
+
+public class kk {
+	public static void main( String []args ) 
+	{
+		System.out.println("Hello. This is a test."); 
+		System.out.print("Vitor ha estado aqui");
+		System.out.println("Interesante.No entiendo why no me salen tus updates en tu rep."); 
+	}
+}
